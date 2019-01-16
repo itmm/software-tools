bb74d27f92ee4a06f4c7dd42c550b232f638b6b7
Timm Knape <timm@knp.de>
2019-01-16 21:36:30 +0100

    better tests

--- tests/test_un-tab.sh
+++ tests/test_un-tab.sh
@@ -7,5 +7,7 @@ function run {
 	diff $expected un-tab-out.txt || exit 1
 }
 
+run test-data/empty.txt test-data/empty.txt
+run test-data/two-lines.txt test-data/two-lines.txt
 run test-data/tabbed.txt test-data/untabbed.txt
 run test-data/tabbed-col.txt test-data/untabbed-col.txt
